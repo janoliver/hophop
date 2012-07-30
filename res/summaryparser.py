04c28235d52f4334bfbcdb95b8b22f00f4b0b547
Jan Oliver Oelerich <janoliver@oelerich.org>
2012-07-30 21:30:04 +0200

    added summary parser class

---
+++ res/summaryparser.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python2
+
+import numpy as np
+import sqlite3 as sql
+
+class SummaryParser(Object):
+    """
+    A parser for the hop summary files. The following must be fulfilled:
+      * The columns MUST be 18 bytes (chars) long
+      * The first line of the file must include the column names
+      * there should not be empty fields in the columns that are read in
+
+      The number of columns to be read in can be specified using the parameter
+      cols of the read function. A typical usage example would be something
+      like this:
+
+        >>> from summaryparser import SummaryParser
+        >>> s = SummaryParser()
+        >>> s.read('summaryfile.dat', cols=30)
+        >>> print s.get_columns()
+        >>> print s.q('SELECT col1, col2 FROM data WHERE col3="val"')
+        >>> s.close()
+    """
+    
+    def __init__(self):
+        self.con = sql.connect(":memory:")
+        self.raw = None
+        self.cols = None
+        
+    def read(self, filename='summary.dat', cols=31):
+        self.raw = np.genfromtxt('compare.dat', dtype=None, delimiter=18,
+                                 autostrip=True, names=True,
+                                 usecols=range(cols),
+                                 comments=None)
+        self.cols = self.raw.dtype.names
+
+        self._insert()
+
+    def q(self, query):
+        return np.array(self.con.execute(query).fetchall())
+
+    def get_columns(self):
+        return self.cols
+
+    def get_raw(self):
+        return self.raw
+    
+    def _insert(self):
+        self.con.execute("create table data({0})".format(",".join(self.cols)))
+        self.con.executemany("insert into data values (%s)" %
+                             ",".join(['?']*len(self.cols)), self.raw.tolist())
+
+    def close(self):
+        self.con.close()
+    
